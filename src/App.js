734debaa7336d80c6b527a718e7a354137c6efd2
WesleyReboucas <wesley_reboucas@hotmail.com>
2021-03-29 12:52:46 -0300

    Validate CPF, part 1

--- src/App.js
+++ src/App.js
@@ -10,9 +10,24 @@ function App() {
       <Typography variant="h3" component="h1" align="center">
         Formulário de Cadastro
       </Typography>
-      <RegistrationForm />
+      <RegistrationForm
+        whenSending={whenSendingForm}
+        validateCPF={validateCPF}
+      />
     </Container>
   );
+}
+
+function whenSendingForm(data) {
+  console.log(data);
+}
+
+function validateCPF(cpf) {
+  if (cpf.length !== 11) {
+    return { valid: false, text: "O CPF deve conter 11 dígitos" };
+  } else {
+    return { valid: true, text: "" };
+  }
 }
 
 export default App;
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,43 +1,42 @@
 import React, { useState } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
-function RegistrationForm() {
+function RegistrationForm({ whenSending, validateCPF }) {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [sales, setSales] = useState(true);
+  const [newsletter, setNewsletter] = useState(true);
+  const [error, setError] = useState({ cpf: { valid: true, text: "" } });
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        console.log(name);
-        console.log(lastName);
+        whenSending({ name, lastName, cpf, sales, newsletter });
       }}
     >
       <TextField
-        value={name}
-        onChange={(event) => {
-          let timeName = event.target.value;
-          if (timeName.length >= 3) {
-            timeName = timeName.substr(0, 3);
-          }
-          setName(timeName);
-        }}
         id="name"
         variant="outlined"
         label="Nome"
         margin="normal"
         fullWidth
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
       />
       <TextField
-        value={lastName}
-        onChange={(event) => {
-          setLastName(event.target.value);
-        }}
         id="last-name"
         variant="outlined"
         label="Sobrenome"
         margin="normal"
         fullWidth
+        value={lastName}
+        onChange={(event) => {
+          setLastName(event.target.value);
+        }}
       />
       <TextField
         id="CPF"
@@ -46,16 +45,44 @@ function RegistrationForm() {
         label="CPF"
         margin="normal"
         fullWidth
+        value={cpf}
+        onChange={(event) => {
+          setCpf(event.target.value);
+        }}
+        onBlur={(event) => {
+          const isValid = validateCPF(cpf);
+          setError({ cpf: isValid });
+        }}
+        error={!error.cpf.valid}
+        helperText={error.cpf.text}
       />
 
       <FormControlLabel
         label="Promoções"
-        control={<Switch name="sales" defaultChecked color="primary" />}
+        control={
+          <Switch
+            name="sales"
+            color="primary"
+            checked={sales}
+            onChange={(event) => {
+              setSales(event.target.checked);
+            }}
+          />
+        }
       />
 
       <FormControlLabel
         label="Novidades"
-        control={<Switch name="newsletter" defaultChecked color="primary" />}
+        control={
+          <Switch
+            name="newsletter"
+            color="primary"
+            checked={newsletter}
+            onChange={(event) => {
+              setNewsletter(event.target.checked);
+            }}
+          />
+        }
       />
 
       <Button type="submit" variant="contained" color="primary">
