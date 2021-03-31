1ab508344f951ea8c0934b7c7de1123d5a17c2a2
WesleyReboucas <wesley_reboucas@hotmail.com>
2021-03-31 15:04:33 -0300

    CPF Validation

--- src/models/registration.js
+++ src/models/registration.js
@@ -1,8 +1,37 @@
 function validateCPF(cpf) {
-  if (cpf.length !== 11) {
-    return { valid: false, text: "O CPF deve conter 11 dígitos" };
+  if (cpf.length === 11) {
+    let validateFirstDigit = 0;
+    let validateSecondDigit = 0;
+
+    // ------------------------------------ VALIDATE 01
+    for (let n = 0; n < 9; n++) {
+      const element = cpf[n];
+      let aux = 10;
+      let validate1 = element * (aux - n);
+      validateFirstDigit = validateFirstDigit + validate1;
+    }
+    validateFirstDigit = (validateFirstDigit * 10) % 11;
+    validateFirstDigit = validateFirstDigit === 10 ? 0 : validateFirstDigit;
+
+    // ------------------------------------ VALIDATE 02
+    for (let n = 0; n < 10; n++) {
+      const element = cpf[n];
+      let aux = 11;
+      let validate1 = element * (aux - n);
+      validateSecondDigit = validateSecondDigit + validate1;
+    }
+    validateSecondDigit = (validateSecondDigit * 10) % 11;
+    validateSecondDigit = validateSecondDigit === 10 ? 0 : validateSecondDigit;
+
+    // ------------------------------------ RESULT
+    // It's necessary use "==", because the operator "===" compare type and value
+    if (validateFirstDigit == cpf[9] && validateSecondDigit == cpf[10]) {
+      return { valid: true, text: "" };
+    } else {
+      return { valid: false, text: "O CPF está incorreto." };
+    }
   } else {
-    return { valid: true, text: "" };
+    return { valid: false, text: "O CPF deve conter 11 dígitos" };
   }
 }
 
